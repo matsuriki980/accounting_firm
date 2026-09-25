e10dd1cb55287eab2f28b54759edcf90931c6b34
matsuyamarikiya <maturiki17@ymail.ne.jp>
2026-09-25 11:59:15 +0900

    add: 共通レイアウト ハンバーガーメニュー [ホバーに応じた画像切り替え処理を実装]

--- assets/js/module/hamburger-menu.js
+++ assets/js/module/hamburger-menu.js
@@ -6,7 +6,6 @@ export function initHamburgerMenu() {
   const hamburgerBtn = document.querySelector(".js-hamburger-btn");
   const hamburgerMenuElement = [header, hamburgerMenu];
   const bodyElement = document.body;
-  let hamburgerMenuPlug = false; // 開閉状態を判定
 
   // ヘッダー背景の初期化
   const updateHeaderBg = initHeaderBg();
@@ -26,5 +25,23 @@ export function initHamburgerMenu() {
 
     // ハンバーガーメニューの状態が変わるのでヘッダー背景も更新する
     updateHeaderBg();
+  });
+
+  // ホバーしたリンクと同じdata値を持つ画像を表示する
+  const hamburgerMenuItem = document.querySelectorAll(
+    ".js-hamburger-menu__item a",
+  );
+  const hamburgerMenuImg = document.querySelectorAll(".js-hamburger-menu__img");
+  hamburgerMenuItem.forEach((item) => {
+    item.addEventListener("mouseenter", () => {
+      hamburgerMenuImg.forEach((img) => {
+        img.classList.remove("is-active");
+      });
+
+      const itemData = item.closest(".js-hamburger-menu__item").dataset.item;
+      const img = hamburgerMenu.querySelector(`[data-img="${itemData}"]`);
+      img.classList.add("is-active");
+      console.log(img);
+    });
   });
 }
